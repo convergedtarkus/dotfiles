5bb160cb3bc2af58c0e217f7a84c0bc03f7955f5
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2019-04-16 20:37:47 -0500

    Fix osx shellcheck issues

--- bash/parts/osSpecific/osx.bash
+++ bash/parts/osSpecific/osx.bash
@@ -7,11 +7,12 @@ alias finder='open -a Finder '
 alias sublime="open -a 'Sublime Text'"
 
 # open the target file in a new tab of mvim
-alias mvi="open \"mvim://open?url=file://$1\""
+alias mvi="open -a MacVim"
 
 # Show/hide hidden files (for Mac OS X Mavericks)
 alias showhidden="defaults write com.apple.finder AppleShowAllFiles TRUE"
 alias hidehidden="defaults write com.apple.finder AppleShowAllFiles FALSE"
 
 # From http://apple.stackexchange.com/questions/110343/copy-last-command-in-terminal
-alias getLastCmd='fc -ln -1 | awk '\''{$1=$1}1'\'' ORS='\'''\'' | pbcopy'
+# Strips the ending newline so the command will not auto run when pasted
+getLastCmd() { fc -ln -1 | awk '{$1=$1}1' | tr -d '\n' | pbcopy; }
