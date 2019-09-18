a493568706db1b3d60548f0fa54e6f8a47d611d8
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2019-09-18 09:52:06 -0500

    Add nuke docker and other docker help function

--- bash/parts/docker.bash
+++ bash/parts/docker.bash
@@ -10,3 +10,50 @@ alias killAllDockerContainers='docker rm -f $(docker ps -aq) >/dev/null 2>&1 || 
 
 # Removes all docker containers. This does not use the force flag, so some images may require manual deletion.
 removeAllDockerContainers() { docker image rm "$(docker image ls | awk '{print $3}')"; }
+
+nukeDocker() {
+	echo
+	echo "Stop all containers"
+	docker stop "$(docker ps -a -q)"
+
+	echo
+	echo "Pruning system"
+	docker system prune --all --force --volumes
+
+	echo
+	echo "Delete all containers"
+	docker rm -f "$(docker ps -a -q)" >/dev/null 2>&1 || true
+
+	echo
+	echo "Delete all images"
+	docker rmi "$(docker images -q)"
+
+	echo
+	echo "Delete all volumes"
+	rm -rf /var/lib/docker/volumes/*
+	rm -rf /var/lib/docker/vfs/dir/*
+
+	echo
+	echo "Kill and restart docker"
+	killall Docker && open /Applications/Docker.app
+
+	echo
+	echo "Delete Final Docker Shiz"
+	rm ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/Docker.qcow2
+
+	echo
+	echo
+	echo "Finished nuking"
+}
+
+dockerStop() {
+	echo "Stop all containers"
+	docker stop "$(docker ps -a -q)"
+	echo "All containers stopped"
+}
+
+dockerKill() {
+	echo "Killing all stuck containers"
+	docker rm -f "$(docker ps -aq)" >/dev/null 2>&1 || true
+	echo "All containers dead"
+}
