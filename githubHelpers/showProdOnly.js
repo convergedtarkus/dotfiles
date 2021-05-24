78068618d4b5ded6f58aa29d8dbfe51fceb429ba
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2021-05-24 09:46:42 -0500

    Add Github helper to only show prod like files
    This will hide/not expand test files, dependency lock files, test util files
    or dependency directories

---
+++ githubHelpers/showProdOnly.js
@@ -0,0 +1,43 @@
+// This script will collapse all not production files for easy viewing.
+// Also collapses any files in a vendor directory or lock/sum files.
+javascript: (function() {
+    // Get all the file names.
+    let allFileHeaders = document.getElementsByClassName("file-header");
+
+    for (let fileHeader of allFileHeaders) {
+        let fileNameElement = fileHeader.getElementsByClassName("Link--primary")[0];
+
+        // Collapse the file if it is test file. Not an exhaustive list (largely Go + Dart specific).
+        // Also collapses dependency files.
+        let shouldShow = true;
+        if (
+            // Direct test files/directories.
+            fileNameElement.textContent.endsWith("_test.go") ||
+            fileNameElement.textContent.includes("/test/") ||
+            // Test util file matches, not perfect but should work pretty well.
+            fileNameElement.textContent.includes("test_utils.") ||
+            fileNameElement.textContent.includes("testutils.") ||
+            // Dependency files/directories.
+            fileNameElement.textContent.startsWith("vendor/") ||
+            fileNameElement.textContent.endsWith(".sum") ||
+            fileNameElement.textContent.endsWith(".lock")) {
+            shouldShow = false;
+        }
+
+        // Correctly check the 'Viewed' button based on shouldShow.
+        let viewedDiff = fileHeader.getElementsByClassName("js-reviewed-checkbox")[0];
+        if (viewedDiff.checked === shouldShow) {
+            viewedDiff.click();
+        }
+
+        // Load large diffs that should be shown as well.
+        if (shouldShow) {
+            let fileContentElement = fileHeader.nextElementSibling;
+            let hiddenDiffReasonElement = fileContentElement.getElementsByClassName("hidden-diff-reason")[0];
+            if (hiddenDiffReasonElement != null && hiddenDiffReasonElement.textContent.includes("Large diffs are not rendered by default.")) {
+                let loadDiffButton = fileContentElement.getElementsByClassName("load-diff-button")[0];
+                loadDiffButton.click();
+            }
+        }
+    }
+})();
