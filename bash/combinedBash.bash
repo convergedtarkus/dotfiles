8036ac345f72a52e9c1f292eb4d03201df348a5e
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2025-03-20 09:07:21 -0500

    Add some go completion logic

--- bash/combinedBash.bash
+++ bash/combinedBash.bash
@@ -26,6 +26,7 @@ source "$MYDOTFILES/bash/parts/dartAndPub.bash"
 # Golang
 # shellcheck source=/dev/null
 source "$MYDOTFILES/bash/parts/go.bash"
+source "$MYDOTFILES/bash/completions/go_completion.bash"
 
 # No support for linux yet, should at least not source this if on linux
 # shellcheck source=/dev/null
