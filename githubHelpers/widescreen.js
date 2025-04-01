319bb6c84727045f1624290c4188df54e9d3a957
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2025-04-01 11:16:18 -0500

    Remove some old and unneeded files

--- githubHelpers/widescreen.js
+++
@@ -1,9 +0,0 @@
-// This script will expand Github pages to use a larger width.
-// The width is controlled by `maxWidth`. Smaller values may be helpful for ultra wide monitors.
-// The default `maxWidth` (98) leaves some buffer space while still being much wider than Github's default.
-javascript: var maxWidth = 98;
-var style = document.createElement('style');
-var padding = (100 - maxWidth) / 2;
-style.innerText = `   .container-lg,   .container-xl {     max-width: ${maxWidth}% !important;     padding-right: ${padding}% !important;     padding-left: ${padding}% !important;   }`;
-var head = document.getElementsByTagName('head')[0];
-head.appendChild(style);
