ba50508d3f5f46f44b71e836c91822f57d3688e9
convergedtarkus <38326544+convergedtarkus@users.noreply.github.com>
2026-05-27 11:00:15 -0500

    Use better blue for file colors

--- bash/parts/osx.bash
+++ bash/parts/osx.bash
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
 
+# Each two letters define the colors.
+#     First is foreground
+#     Second is background
+#
+# 1.  Directory
+# 2.  Symbolic link
+# 3.  Socket
+# 4.  Pipe
+# 5.  Executable
+# 6.  Block special
+# 7.  Character special
+# 8.  Executable with setuid bit set
+# 9.  Executable with setgid bit set
+# 10. Directory writable to others, with sticky bit
+# 11. Directory writable to others, without sticky bit
+#
+# a = black
+# b = red
+# c = green
+# d = brown
+# e = blue
+# f = magenta
+# g = cyan
+# h = light grey
+# A = bold black, usually shows up as dark grey
+# B = bold red
+# C = bold green
+# D = bold brown, usually shows up as yellow
+# E = bold blue
+# F = bold magenta
+# G = bold cyan
+# H = bold light grey; looks like bright white
+# x = default foreground or background
+export LSCOLORS="Exfxcxdxbxegedabagacad"
+
 # Check if we're on Mac OS X before defining any of these aliases or functions.
 if [[ "$(uname)" != "Darwin" ]]; then
 	return
